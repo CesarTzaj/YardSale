44db944004f538ef26df13105fe905838b978a00
Cesar Tzaj <cesar.tzaj@outlook.com>
2022-11-04 15:17:23 -0600

    se agrego el menu de yard sale

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 const btnnemu = document.querySelector('.navbar-email');
 const btnnemuPhone = document.querySelector('.menu');
 const btnshopping_cart = document.querySelector('.navbar-shopping_cart');
+const btnproductDetailClose = document.querySelector('.product-detail-close');
 
 const menuEmail = document.querySelector('.desktop-menu');
 const menuPrincipal = document.querySelector('.navbar-left');
-const product_detail = document.querySelector('.product-detail');
+const shoppingCartDetails = document.querySelector('.shoppingCardDetails');
+const productDetailAside = document.querySelector('.productDetailAside');
+const mainfilter = document.querySelector('main')
 var cardsContainer = document.querySelector('.cards-container');
 
 btnnemu.addEventListener('click', function(){agregarmenu(menuEmail)});
 btnnemuPhone.addEventListener('click', function(){agregarmenu(menuPrincipal)});
-btnshopping_cart.addEventListener('click', function(){agregarmenu(product_detail)});
+btnshopping_cart.addEventListener('click', function(){agregarmenu (shoppingCartDetails)});
+btnproductDetailClose.addEventListener('click', cerrarProductDetail);
 
 function agregarmenu(menu){
     if(!menu.classList.contains('show')){
         menuEmail.classList.remove('show');
-        product_detail.classList.remove('show');
+        shoppingCartDetails.classList.remove('show');
         menuPrincipal.classList.remove('show');
+        productDetailAside.classList.remove('show');   
+        mainfilter.classList.remove('show')
     }
     menu.classList.toggle('show');
 }
+
+function sidemenu(){
+    menuEmail.classList.remove('show');
+    shoppingCartDetails.classList.remove('show');
+    menuPrincipal.classList.remove('show');
+    productDetailAside.classList.add('show')
+    mainfilter.classList.add('show')
+}
+
+ function cerrarProductDetail(){
+     productDetailAside.classList.remove('show');
+     mainfilter.classList.remove('show')
+ }
 
 var productList = [];
 productList.push({
@@ -45,7 +64,7 @@ productList.push({
     Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     shoppingCart: './icons/bt_add_to_cart.svg'
 });
-
+//primera forma de poder ingresar la tarjeta de html
 
 function  productRender(ArrayProductos){
     for(product of ArrayProductos){
@@ -54,6 +73,8 @@ function  productRender(ArrayProductos){
 
         const productImage = document.createElement('img');
         productImage.setAttribute('src', product.Image);
+
+        productImage.addEventListener('click', sidemenu)
 
         const product_info = document.createElement('div');
         product_info.classList.add('product-info');
@@ -76,74 +97,4 @@ function  productRender(ArrayProductos){
 
     }
 }
-//  productRender(productList);
-function ProductoRender(arralist){
-    for(product of arralist){
-        const htmlCards= `<div class="product-card">
-        <img src=${product.Image} alt="">
-        <div class="product-info">
-        <div>
-                <p>$ ${product.price}</p>
-                <p>${product.name}</p>
-              </div>
-              <figure>
-                <img src="./icons/bt_add_to_cart.svg" alt="">
-              </figure>
-            </div>
-          </div>`;
-          const  cardsContainers = document.querySelector('.cards-container');
-          cardsContainers.innerHTML += htmlCards;
-    }
-}
-ProductoRender(productList);
-// for(let i=0; i<productList.length; i++){
-//     console.log(productList[i]);
-//     console.log(productList.name);
-// }
-// for(productos of productList){
-//     console.log(productos.name)
-// }
-// for(productos in productList){
-//     console.log(productos)
-// }
-
-var frutas = ['manzanas','peras','plantano','fresa'];
-frutas.push('uvas') // agrega un elemento en la ultimo posicion
-frutas.pop(); // eliminia el ultimo elemento
-frutas.unshift('mango'); // agrega un elemento en su primera posicion
-frutas.shift(); // elimina el primer elemento en el array
-console.log(frutas.indexOf('manzanas'))
-
-frutas.length
-const items =[ 
-    {name: 'Bike',    price: 10 },
-    {name: 'Tvi',     price: 20 },
-    {name: 'Album',   price: 10  },
-    {name: 'Book',    price: 20   },
-    {name: 'Phone',   price: 50 }
-];
-// const filteredItems = items.filter(function(item){
-//     return item.price<=100;
-// });
- const filteredItems = items.filter((item)=>{
-     return item.price<=100;
-  });
- const itemsNmae = items.map((item)=> item.name);
- const itemFind = items.find((item) => item.name=="Book");
-//  items.forEach((item) => console.log(item.name));
- const hasInexpensiveItems = items.some((item) =>{
-     return item.price <=100;
- });
- const array1 = [1, 2, 3, 4];
- const hasexpensiveItems = items.every((item) => item.price == 100);
-const total = items.reduce(( currentTotal, item) =>   item.price + currentTotal ,0); //para hacer sumas
-const includeten = array1.includes(10);
-
-// console.log(items);
-//  console.log(filteredItems);
-// console.log(itemsNmae)
-// console.log(itemFind)
-// console.log(hasInexpensiveItems)
-// console.log(hasexpensiveItems)
-// console.log(total)
-// console.log(includeten)
+  productRender(productList);
